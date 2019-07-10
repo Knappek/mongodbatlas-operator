7ceefe622f112bd2211082bd1b6dd2e5528b86a2
andreas.knapp <andy.knapp.ak@gmail.com>
2019-07-10 19:26:43 +0200

    test non-existing MongoDBAtlasProject CR

--- pkg/controller/mongodbatlasproject/mongodbatlasproject_controller_test.go
+++ pkg/controller/mongodbatlasproject/mongodbatlasproject_controller_test.go
@@ -33,6 +33,71 @@ var (
 	clusterCount   = 0
 )
 
+func TestNonExistingMongoDBAtlasProjectCR(t *testing.T) {
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(logf.ZapLogger(true))
+
+	// A MongoDBAtlasProject resource with metadata and spec.
+	mongodbatlasproject := &knappekv1alpha1.MongoDBAtlasProject{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      projectName,
+			Namespace: namespace,
+		},
+		Spec: knappekv1alpha1.MongoDBAtlasProjectSpec{
+			OrgID: organizationID,
+		},
+	}
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		mongodbatlasproject,
+	}
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(knappekv1alpha1.SchemeGroupVersion, mongodbatlasproject)
+
+	// Create a fake k8s client to mock API calls.
+	k8sClient := fake.NewFakeClient(objs...)
+	// Create a fake atlas client to mock API calls.
+	// atlasClient, server := test.NewAtlasFakeClient(t)
+	httpClient, mux, server := testutil.Server()
+	defer server.Close()
+	// getByName: assert that there is no existing project
+	mux.HandleFunc("/api/atlas/v1.0/groups/byName/"+projectName, func(w http.ResponseWriter, r *http.Request) {
+		testutil.AssertMethod(t, "GET", r)
+		fmt.Fprintf(w, "")
+	})
+	// Post
+	mux.HandleFunc("/api/atlas/v1.0/groups/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.AssertMethod(t, "POST", r)
+		w.Header().Set("Content-Type", "application/json")
+		expectedBody := map[string]interface{}{
+			"orgId": organizationID,
+			"name":  projectName,
+		}
+		testutil.AssertReqJSON(t, expectedBody, r)
+		fmt.Fprintf(w, `{"clusterCount": `+strconv.Itoa(clusterCount)+`, "created":"`+created+`", "id":"`+projectID+`", "links":[], "name":"`+projectName+`", "orgId":"`+organizationID+`"}`)
+	})
+	atlasClient := ma.NewClient(httpClient)
+
+	// Create a ReconcileMemcached object with the scheme and fake client.
+	r := &ReconcileMongoDBAtlasProject{client: k8sClient, scheme: s, atlasClient: atlasClient}
+
+	// Mock request with non-existing project
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "non-existent-project",
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	assert.Equal(t, false, res.Requeue)
+	assert.Equal(t, reconcile.Result{}, res)
+}
+
 func TestCreateMongoDBAtlasProject(t *testing.T) {
 	// Set the logger to development mode for verbose logs.
 	logf.SetLogger(logf.ZapLogger(true))
@@ -95,10 +160,7 @@ func TestCreateMongoDBAtlasProject(t *testing.T) {
 	if err != nil {
 		t.Fatalf("reconcile: (%v)", err)
 	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if res.RequeueAfter != time.Second*30 {
-		t.Errorf("reconcile did not requeue request as expected after 30 seconds: %v", res.RequeueAfter)
-	}
+	assert.Equal(t, time.Second*30, res.RequeueAfter)
 
 	// Check if the CR has been created and has the correct status.
 	cr := &knappekv1alpha1.MongoDBAtlasProject{}
@@ -181,14 +243,8 @@ func TestDeleteMongoDBAtlasProject(t *testing.T) {
 	if err != nil {
 		t.Fatalf("reconcile: (%v)", err)
 	}
-	if res != (reconcile.Result{}) {
-		t.Error("reconcile did not return an empty Result")
-	}
-
-	// Check the result of reconciliation to make sure it has the desired state.
-	if res.Requeue {
-		t.Errorf("reconcile did requeue request as expected: %v", res.Requeue)
-	}
+	assert.Equal(t, reconcile.Result{}, res)
+	assert.Equal(t, false, res.Requeue)
 
 	// Check if the CR has been created and has the correct status.
 	cr := &knappekv1alpha1.MongoDBAtlasProject{}
